76a2002977cb1f4a0434593a339d97bc0aa16626
Nargis <nargis.salim@btinternet.com>
2021-08-07 21:42:20 +0100

    introduce self and print tree(s)

---
+++ Tree.java
@@ -0,0 +1,62 @@
+// Planting a Tree
+
+// introduce self and print tree(s)
+
+/* KEY CONCEPTS
+Java naming conventions
+the main() method
+println() functions
+comments
+*/
+
+// class name matches file name
+// The main() method runs the tasks of the program
+public class Tree{
+  public static void main(String[] args){
+    System.out.println("Hello, this is Miss Sunshine!");
+    System.out.println("Keep calm and plant a tree!");
+    System.out.println("Be ready to get your hands dirty! The planet needs you!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+    System.out.println("==========");
+
+    System.out.println("  *****   ");
+    System.out.println(" ***/**** ");
+    System.out.println("***|/**** ");
+    System.out.println("  *||/**  ");
+    System.out.println("   ||*    "); 
+    System.out.println("   ||     "); 
+    System.out.println("#######   "); 
+
+    System.out.println("* ** *");
+    System.out.println("******");
+    System.out.println(" **** ");
+    System.out.println("  **  ");
+    System.out.println("  **  "); 
+    System.out.println("~~~~~~~~~~"); 
+
+    System.out.println("*****");
+    System.out.println("*****");
+    System.out.println("*****");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+    System.out.println("#######"); 
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
